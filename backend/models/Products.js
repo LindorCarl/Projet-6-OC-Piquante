a6ea0d9857af94e20eea5f415fd2098f0dd797e0
LindorCarl <lindorcarl8@gmail.com>
2023-03-30 03:36:37 +0200

    first commit

--- backend/models/Products.js
+++ backend/models/Products.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 
 //Créér un schéma pour enregistrer les produits dans la database.
 const productsSchema = mongoose.Schema({
-    userId: {type : String},
-    name: {type : String},
-    manufacturer: {type : String},
-    description: {type : String},
-    mainPepper: {type : String},
-    imageUrl: {type : String}, 
-    heat: {type : Number}, 
-    likes: {type : Number},  
-    dislikes: {type : Number}, 
+    userId: {type : String, required: true},
+    name: {type : String, required: true},
+    manufacturer: {type : String, required: true},
+    description: {type : String, required: true},
+    mainPepper: {type : String, required: true},
+    imageUrl: {type : String, required: true}, 
+    heat: {type : Number, required: true}, 
+    likes: {type : Number, default: 0},  
+    dislikes: {type : Number, default: 0}, 
     usersLiked: {type : [String]}, 
     usersDisliked: {type : [String]} 
 },
